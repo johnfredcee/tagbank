0f6c95a4c7ad54a7e22016fa0b03890dd9289323
John Connors <johnc@yagc.ndo.co.uk>
2016-10-30 00:00:53 +0100

    Added placeholder for watch script

---
+++ tagwatch.py
@@ -0,0 +1,16 @@
+# -*- mode: Python; tab-width: 4 -*-
+#
+# Transform an euberant ctags file to a sqlite database
+#
+import sys
+import ctags
+import getopt
+import os
+import os.path
+import subprocess
+import configparser
+import sqlite3
+import watchdog
+
+if __name__ == "__main__":
+    print("When I grow up, I hope to get implemented")
